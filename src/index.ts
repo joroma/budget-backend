955be05c01a7b636dae218e0965c9603bbe59030
John Roger Maglalang <john.maglalang@transcom.com>
2025-01-05 05:35:52 +0800

    Update logger

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import express, { Express, Request, Response } from "express";
 import config from "./providers/config";
-import { logger } from "./common/utils/logger";
+import bodyParser from "body-parser";
+import { logger } from "./utils/logger";
+import accountRouter from "./routes/account.router";
+import { httpLogger } from "./middlewares/http.logger";
 
 const app: Express = express();
 
+// Add body parser
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(httpLogger);
 app.get("/", (req: Request, res: Response) => {
   res.send("budget let's goooo!!!!!");
 });
 
+app.use(accountRouter);
+
 app.listen(config.PORT, () => {
-  logger.info(`\uf00c [server]: Server is running at http://localhost:${config.PORT}`);
-}); 
+  logger.info(
+    `\uf00c [server]: Server is running at http://localhost:${config.PORT}`,
+  );
+});
